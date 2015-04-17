421e32e52fb75228ff546cbb58b887bf1516627e
rosstapson <ross.tapson@gmail.com>
2015-04-17 05:57:42 +0200

    android implementation

--- inspector/an-inspector/ForgeModule/src/io/trigger/forge/android/modules/display/API.java
+++ inspector/an-inspector/ForgeModule/src/io/trigger/forge/android/modules/display/API.java
@@ -1,9 +1,13 @@
 package io.trigger.forge.android.modules.display;
 
 import io.trigger.forge.android.core.ForgeApp;
+import io.trigger.forge.android.core.ForgeParam;
 import io.trigger.forge.android.core.ForgeTask;
 import android.content.pm.ActivityInfo;
+import android.graphics.Color;
 import android.os.Build;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class API {
 	public static void orientation_forcePortrait(final ForgeTask task) {
@@ -37,9 +41,29 @@ public class API {
 	public static void setStatusBarColour(final ForgeTask task, @ForgeParam("text") final String bgColour) {
 		ForgeApp.getActivity().runOnUiThread(new Runnable() {
         public void run() {
-                whatever nefarious thing you're doin'
+        	if (bgColour.length() == 0) {
+        		task.error("invalid bgColour string: zero length");
+        		return;
+        	}
+        	int colourInt = -1;
+        	try {
+        		colourInt = Color.parseColor(bgColour);
+        	}
+        	catch (IllegalArgumentException e) {
+        		task.error("invalid bgColour string: " + bgColour + " valid e.g. '#ffff00'");
+        		return;
+        	}
+        	if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+        	    Window window = ForgeApp.getActivity().getWindow();
+        	    window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+        	    window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        	    window.setStatusBarColor(colourInt);
+        	}
         }
-});
+		});
+		if (task != null) {
+			task.success();
+		}
 	}
 
 }
